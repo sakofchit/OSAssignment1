304dd5d73486066345552f26f41b1d146bad2a6e
sakofchit <sakun.acharige@gmail.com>
2023-02-28 00:57:32 -0500

    fix formatting

--- lkmasg1.c
+++ lkmasg1.c
@@ -151,29 +151,29 @@ static ssize_t read(struct file *filep, char *buffer, size_t len, loff_t *offset
 	int error_count = 0;
 	char temp[sizeof(message)] = {0}; // temporary array to hold message without read data
 	int i;
-	
+
 	// If not enough data is available to service a read request, the driver must respond with only the amount available (including 0 bytes).
 	if(len > size_of_message)
 		len = size_of_message;
-    
-   	error_count = copy_to_user(buffer, message, len);
- 
-   	if (error_count == 0) {           // if true then have success
+
+	error_count = copy_to_user(buffer, message, len);
+
+	if (error_count == 0) {           // if true then have success
 		printk(KERN_INFO "lkmasg1: Sent %zu characters to the user: %s\n", len, buffer);
-		
+
 		int j = 0;
 		for(i = len; i < size_of_message; i++) {
-		    temp[j] = message[i];
+			temp[j] = message[i];
 			j++;
-        }
-        
-        size_of_message = size_of_message - len; // update the size of the message
-        strcpy(message, temp);					 // copy temp to message
+		}
 
-        return 0;
-   	}
-   	
-   	else {
+		size_of_message = size_of_message - len; // update the size of the message
+		strcpy(message, temp);					 // copy temp to message
+
+		return 0;
+	}
+
+	else {
 		printk(KERN_INFO "lkmasg1: Failed to send %d characters to the user\n", error_count);
 		return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
    	}
@@ -187,16 +187,16 @@ static ssize_t write(struct file *filep, const char *buffer, size_t len, loff_t 
 {
 
 	printk(KERN_INFO "write stub");
-	
+
 	int max_message_size = (int)(sizeof(message) - size_of_message - 1); // -1 for null terminator
 
 	// If not enough buffer is available to store a write request, the driver must store only up to the amount available
-   	if (len > max_message_size)
+	if (len > max_message_size)
 		len = max_message_size;
 
 	strncat(message, buffer, len); 			// appending received string with its length
-   	size_of_message = strlen(message); 		// store the length of the stored message
-   	printk(KERN_INFO "lkmasg1: Received %zu characters from the user\n", len);
+	size_of_message = strlen(message); 		// store the length of the stored message
+	printk(KERN_INFO "lkmasg1: Received %zu characters from the user\n", len);
 
 	return len;
 
